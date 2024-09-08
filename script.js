2b8c78994e439d050a9f62438bcd5a99791861d6
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-09-08 23:03:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,5 +29,12 @@ function draw() {
 }
 
 function calculateState(a, b, c) {
-
+    if (a == 1 && b == 1 && c == 1) return 1;
+    if (a == 1 && b == 1 && c == 0) return 0;
+    if (a == 1 && b == 0 && c == 1) return 1;
+    if (a == 1 && b == 0 && c == 0) return 1;
+    if (a == 0 && b == 1 && c == 1) return 0;
+    if (a == 0 && b == 1 && c == 0) return 0;
+    if (a == 0 && b == 0 && c == 1) return 1;
+    if (a == 0 && b == 0 && c == 0) return 0;
 }
